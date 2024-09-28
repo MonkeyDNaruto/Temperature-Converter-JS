ae6be25a94e7edbdcbf19018e71c2b49cf52ea34
Nischal Shrestha <nischalshrestha324@gmail.com>
2024-09-28 19:50:47 +0545

    Temperature converter app incomplete

---
+++ js/app.js
@@ -0,0 +1,12 @@
+const primaryInput = document.querySelector('#primary-input')
+const secondaryInput = document.querySelector('#secondary-input')
+const primarySelect = document.querySelector('#primary-units')
+const secondarySelect = document.querySelector('#secondary-units')
+
+primaryInput.value = 0
+secondaryInput.value = 0 
+
+console.log('primaryInput', primaryInput.value)
+console.log('secondaryInput', secondaryInput.value)
+console.log('primarySelect', primarySelect.value)
+console.log('secondarySelect', secondarySelect.value)

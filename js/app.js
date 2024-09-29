db736dcf520003e037034949100e363c69129c61
Nischal Shrestha <nischalshrestha324@gmail.com>
2024-09-29 20:50:06 +0545

    Temperature converter app completed

--- js/app.js
+++ js/app.js
@@ -1,12 +1,58 @@
-const primaryInput = document.querySelector('#primary-input')
-const secondaryInput = document.querySelector('#secondary-input')
-const primarySelect = document.querySelector('#primary-units')
-const secondarySelect = document.querySelector('#secondary-units')
+const primaryInput = document.querySelector("#primary-input");
+const secondaryInput = document.querySelector("#secondary-input");
+const primarySelect = document.querySelector("#primary-units");
+const secondarySelect = document.querySelector("#secondary-units");
 
-primaryInput.value = 0
-secondaryInput.value = 0 
+primaryInput.value = 0;
+secondaryInput.value = 0;
 
-console.log('primaryInput', primaryInput.value)
-console.log('secondaryInput', secondaryInput.value)
-console.log('primarySelect', primarySelect.value)
-console.log('secondarySelect', secondarySelect.value)
+function update(e) {
+  const elementId = e.target.id;
+
+  if (elementId === "secondary-input") {
+    primaryInput.value = calculate(
+      secondarySelect.value,
+      primarySelect.value,
+      secondaryInput.value
+    );
+  } else {
+    secondaryInput.value = calculate(
+      primarySelect.value,
+      secondarySelect.value,
+      primaryInput.value
+    );
+  }
+}
+
+function calculate(firstTempUnit, secondTempUnit, temp) {
+  const combination = firstTempUnit + "-" + secondTempUnit;
+  let result;
+  switch (combination) {
+    case "fahrenheit-celcius":
+      result = ((Number(temp) - 32) * 5) / 9;
+      break;
+    case "celcius-fahrenheit":
+      result = Number((temp * 9) / 5) + 32;
+      break;
+    case "kelvin-fahrenheit":
+      result = ((Number(temp) - 273.15) * 9) / 5 + 32;
+      break;
+    case "kelvin-celcius":
+      result = Number(temp) - 273.15;
+      break;
+    case "fahrenheit-kelvin":
+      result = ((Number(temp) - 32) * 5) / 9 + 273.15;
+      break;
+    case "celcius-kelvin":
+      result = Number(temp) + 273.15;
+      break;
+    default:
+      result = Number(temp);
+      break;
+  }
+  return result.toFixed(2);
+}
+
+[primaryInput, secondaryInput, primarySelect, secondarySelect].forEach(
+  (element) => element.addEventListener("change", update)
+);
